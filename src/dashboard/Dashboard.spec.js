e2fb6e184d8ac55c5e77780565f5027c489fb693
steelworx <eileencuevas62@gmail.com>
2019-01-30 17:03:25 -0500

    added tests to prove that Dashboard shows controls and display

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,16 @@
-// Test away
+import React from 'react';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import Dashboard from './Dashboard';
+
+describe('Dashboard Tests', () => {
+    it('should show the controls and display', () => {
+        const { getByTestId } = render(<Dashboard />);
+
+        getByTestId('locked-display');
+        getByTestId('closed-display');
+        getByTestId('close-btn');
+        getByTestId('lock-btn');
+    })
+})
