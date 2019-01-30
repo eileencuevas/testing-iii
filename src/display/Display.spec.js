e19f6ec4a85f3769a3724c9613bbbad74bf25cb0
steelworx <eileencuevas62@gmail.com>
2019-01-30 16:54:19 -0500

    added tests to prove that displays are shown and change text displayed

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,48 @@
-// Test away!
+import React from 'react';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import Display from './Display';
+
+describe('Display Test', () => {
+    afterEach(cleanup);
+
+    it(`should have two displays`, () =>{
+        const { getByTestId } = render(<Display closed={true} />);
+
+        getByTestId('closed-display');
+        getByTestId('locked-display');
+    })
+
+    it(`should display 'closed' when props.closed is true`, () =>{
+        const { getByTestId } = render(<Display closed={true} />);
+
+        const closedDisplay = getByTestId('closed-display');
+
+        expect(closedDisplay).toHaveTextContent('Closed');
+    })
+
+    it(`should display 'open' when props.closed is false`, () =>{
+        const { getByTestId } = render(<Display />);
+
+        const closedDisplay = getByTestId('closed-display');
+
+        expect(closedDisplay).toHaveTextContent('Open');
+    })
+
+    it(`should display 'locked' when props.locked is true`, () =>{
+        const { getByTestId } = render(<Display locked={true} />);
+
+        const lockedDisplay = getByTestId('locked-display');
+
+        expect(lockedDisplay).toHaveTextContent('Locked');
+    })
+
+    it(`should display 'unlocked' when props.locked is false`, () =>{
+        const { getByTestId } = render(<Display />);
+
+        const lockedDisplay = getByTestId('locked-display');
+
+        expect(lockedDisplay).toHaveTextContent('Unlocked');
+    })
+})
